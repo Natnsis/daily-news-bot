de3e83834e072ce42d03a819d33194b0b582a2b7
Nati <nsisay49@gmail.com>
2026-02-10 13:21:41 +0300

    server conosle fix

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (_req, res) => {
+  res.send('OK');
+});
+
 app.listen(3000, () => {
   console.log('server running on port:3000');
-})
+});
